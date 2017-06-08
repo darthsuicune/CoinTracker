522100e10609cdba682f21998e33ae7d89dad761
Denis Lapuente <darthsuicune@gmail.com>
2017-06-08 14:24:48 +0200

    Initial commit

---
+++ app/src/main/java/com/dlgdev/cointracker/AddCoinActivity.kt
@@ -0,0 +1,12 @@
+package com.dlgdev.cointracker
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AddCoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_coin)
+    }
+}
---
+++ app/src/main/java/com/dlgdev/cointracker/CoinListActivity.kt
@@ -0,0 +1,37 @@
+package com.dlgdev.cointracker
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_coin_list.*
+
+class CoinListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { _ -> val intent = Intent(this, AddCoinActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_coin_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
